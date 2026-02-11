a5044795b05a838100bead6397bf9e3e3388138a
yassigness <zouhirmarjane2@gmail.com>
2026-02-11 19:28:32 +0100

    more content

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 var state_machine
 var player = null
 @export var speed : float
-@onready var player_path = "/root/main/game/player"
+@onready var player_path = "/root/main/player"
 @export var nav_agent : NavigationAgent3D
 @export var anime_tree : AnimationTree
 @export var attack_range : float
@@ -14,9 +14,11 @@ var player = null
 @export var zomnie_sound: AudioStreamPlayer
 @export var death: AudioStreamPlayer
 @export var punch: AudioStreamPlayer
-@onready var player_healt = "/root/main/game/player/code logic/healt"
+@export var health_bar : ProgressBar
+@onready var player_healt = "/root/main/player/code logic/healt"
 var healt_node
-var health = 200
+var health 
+var full_health
 var is_hit = false
 var dami = 2
 var is_dead = false
@@ -27,30 +29,33 @@ var sound_is_playing = false
 
 func _ready() -> void:
 	health = randf_range(180 , 400)
+	full_health = health
 	scale = Vector3(health / 200 , health / 200 , health / 200)
 	player = get_node(player_path)
 	healt_node = get_node(player_healt)
 	state_machine = anime_tree.get("parameters/playback")
 	var score_label = get_node("/root/main/ui/healthetc/Panel/Label")
 	dead.connect(score_label._on_zombie_dead)
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void :
 	velocity = Vector3.ZERO
+	health_bar.value = health / full_health * 100
 
 	i += delta
 	if i > 1.0:
-		if randi() % 100 + 1 >= 85 and not zomnie_sound.playing:
+		var ii = randi() % 100 + 1
+		if ii >= 85 and not zomnie_sound.playing:
 			zomnie_sound.play()
 			
-		elif randi() % 100 + 1 < 85:
+		else :
 			zomnie_sound.stop()
 		i = 0.0
 	
 	if not is_hit :
 		match state_machine.get_current_node() :
 			"run":
-				nav_agent.set_target_position(player.position)
+				nav_agent.set_target_position(player.global_position)
 				var next_nav_point = nav_agent.get_next_path_position()
-				velocity = (next_nav_point - position).normalized() * speed * delta
+				velocity = (next_nav_point - global_position).normalized() * speed * delta
 				rotation.y = lerp_angle(rotation.y , atan2(-velocity.x , -velocity.z) , delta * 10)
 			"punch" :
 				look_at(Vector3(player.global_position.x , global_position.y , player.global_position.z) , Vector3.UP)
@@ -61,6 +66,7 @@ func _process(delta: float) -> void:
 	anime_tree.set("parameters/conditions/punch" , _target_in_range())
 	anime_tree.set("parameters/conditions/run" , !_target_in_range())
 	if  health <= 0 and not is_dead  :
+			remove_from_group("enemy")
 			is_dead = true
 			anime_tree.set("parameters/conditions/die" , true)
 			dead.emit()
