164b5116ba79bf9ad2ab47e35c745e1565df50f8
yassigness <zouhirmarjane2@gmail.com>
2026-02-14 13:52:10 +0100

    added revolver

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,12 +3,31 @@ extends Node3D
 @export var hit_rec : ColorRect
 @export var spawns : Node3D
 @export var nav_reg : NavigationRegion3D
-
+@export var cross_hair : TextureRect
+@export var timer : Timer
+@export var timer_label : Label
+@export var enemy_remaining : Label
 var zombie = load("res://scene/zombie.scn")
 var inst
+var timer_started = false
+var wave = 0
+
 
 func _ready() -> void:
 	randomize()
+	cross_hair.position.x = get_viewport().size.x / 2 - 32
+	cross_hair.position.y = get_viewport().size.y / 2 - 32
+
+
+func _process(delta: float) -> void:
+	var enemy = get_tree().get_nodes_in_group("enemy")
+	enemy_remaining.text = str(enemy.size())
+	timer_label.text = "next wave in : " + str("%0.2f" % timer.time_left)
+	if enemy.is_empty() and not timer_started :
+		timer.start()
+		timer_started = true
+
+
 
 func _on_healt_player_hit() -> void:
 	hit_rec.visible = true
@@ -19,8 +38,13 @@ func get_random_child(parent_node) :
 	var randid = randi() % parent_node.get_child_count()
 	return parent_node.get_child(randid)
 
-func _on_timer_timeout() -> void:
-	var spawn_point = get_random_child(spawns).global_position
-	inst = zombie.instantiate()
-	nav_reg.add_child(inst)
-	inst.global_position = spawn_point
+func _on_timer_2_timeout() -> void:
+	wave += randi_range(1 , 4)
+	timer.stop()
+	for i in wave :
+		var spawn_point = get_random_child(spawns).global_position
+		inst = zombie.instantiate() 
+		inst.add_to_group("enemy")
+		nav_reg.add_child(inst)
+		inst.global_position = spawn_point
+	timer_started = false
--- scripts/shot.gd
+++ scripts/shot.gd
@@ -14,7 +14,12 @@ extends State
 @export var reload_revolver : AudioStreamPlayer
 @export var reload_ : AudioStreamPlayer
 @export var abylity_bar : ProgressBar
+@export var ability : Panel
 @export var head : Node3D
+var total_magazin: int = 200
+var total_revolver_magazin: int = 24
+var revolver_shots = 0
+var shots = 0
 var ability_timer = 0
 signal mele_hit()
 
@@ -26,10 +31,25 @@ var instance2
 @export var label : Label
 var is_reloading = false
 var main = true
+var max_ = magzin_number
+var max_revolver = revolver_magzin_number
 var magazin : int = magzin_number
 var revolver_magazine : int = revolver_magzin_number
 
+
 func process_physics(delta: float) -> State:
+	if total_magazin <= 0 :
+		total_magazin =0
+		magzin_number = 0
+	elif total_magazin < magzin_number :
+		magzin_number = total_magazin
+	if total_revolver_magazin <= 0 :
+		total_revolver_magazin =0
+		revolver_magzin_number = 0
+	elif total_revolver_magazin < revolver_magzin_number :
+		revolver_magzin_number = total_revolver_magazin
+
+
 	if ability_timer > 0 :
 		ability_timer -= delta
 		abylity_bar.value += delta * 13.33
@@ -39,10 +59,10 @@ func process_physics(delta: float) -> State:
 
 
 
-	if magazin <= 0 :
+	if magazin <= 0 and not is_reloading and total_magazin > 0:
 		magazin = 0
 		reload()
-	if revolver_magazine <= 0 :
+	if revolver_magazine <= 0 and not is_reloading and total_revolver_magazin > 0:
 		revolver_magazine = 0
 		revolver_reload()
 
@@ -56,77 +76,78 @@ func process_physics(delta: float) -> State:
 		second_gun_swap()
 
 	if main :
-		label.text = str(magazin)
-		if Input.is_action_just_pressed("ability") and not is_reloading and ability_timer <= 0:
-			parent.position += head.transform.basis * Vector3.FORWARD * 20
-			ability_timer = 7.5
-			abylity_bar.value = 0
-		if Input.is_action_pressed("shoot") and not is_reloading:
-			shoot(delta)
+		ability.visible = false
+		label.text = str(magazin) + "/" + str(total_magazin)
+		clamp(magazin , 0 , max_)
+		if Input.is_action_pressed("shoot") and not is_reloading :
+			shoot()
+			camera.rotation = lerp(camera.rotation , Vector3(
+				camera.rotation.x + randf_range(-0.5 , 3) * delta , camera.rotation.y + randf_range(-2 , 2) * delta , 0 ) , delta * 8)
 		else :
 			shot_.stop()
-		if Input.is_action_just_pressed("reload"):
+		if Input.is_action_just_pressed("reload") and total_magazin > 0 and magazin < max_:
 			reload()
 	else :
-		label.text = str(revolver_magazine)
+		ability.visible = true
+		label.text = str(revolver_magazine) + "/" + str(total_revolver_magazin)
+		clamp(revolver_magazine , 0 , max_revolver)
 		shot_.stop()
 		if Input.is_action_just_pressed("ability") and not is_reloading and ability_timer <= 0:
 			while revolver_magazine > 0 :
-				revolver_shot(delta)
-				await get_tree().create_timer(0.2).timeout
-				ability_timer = 7.5
-				abylity_bar.value = 0
+				revolver_shot()
+				await get_tree().create_timer(0.01).timeout
+			ability_timer = 7.5
+			abylity_bar.value = 0
 		if Input.is_action_just_pressed("shoot") and not is_reloading:
-			revolver_shot(delta)
+			revolver_shot()
 			is_reloading = true
 			await get_tree().create_timer(0.4).timeout
 			is_reloading = false
-		if Input.is_action_just_pressed("reload"):
+		if Input.is_action_just_pressed("reload") and total_revolver_magazin > 0 and revolver_magazine < max_revolver:
 			revolver_reload()
 
-	if Input.is_action_just_pressed("melee") :
-		var rand = randf()
-		if rand <= 0.5 :
-			gun_anime2.play("melee")
-		else :
-			gun_anime.play("melee")
 	return shot
 
 func reload() :
 	is_reloading = true
 	gun_anime2.play("reload")
 	gun_anime.play("reload")
-	reload_.pitch_scale = randf_range(0.8, 1.2)
+	reload_.pitch_scale = randf_range(0.4, 0.8)
 	reload_.play()
 	await get_tree().create_timer(1.2).timeout
-	magazin = magzin_number
+	magazin += shots
 	is_reloading = false
+	total_magazin -= shots
+	shots = 0
 
-func shoot(delta):
-	if !gun_anime.is_playing() :
-		magazin -= 1
-		gun_anime.play("shoot")
-		shot_.pitch_scale = randf_range(0.8, 1.2)
-		shot_.play()
-		instance = buleet.instantiate()
-		instance.position = bareel.global_position
-		instance.transform.basis = bareel.global_transform.basis
-		parent.get_parent().add_child(instance)
-	if !gun_anime2.is_playing() :
-		magazin -= 1
-		gun_anime2.play("shoot")
-		instance2 = buleet.instantiate()
-		instance2.position = bareel2.global_position
-		instance2.transform.basis = bareel2.global_transform.basis
-		parent.get_parent().add_child(instance2)
-		camera.rotation = lerp(camera.rotation , Vector3(
-			camera.rotation.x + randf_range(-0.5 , 3) * delta , camera.rotation.y + randf_range(-2 , 2) * delta , 0 ) , delta * 8)
+func shoot():
+	if magazin > 0 :
+		if !gun_anime.is_playing() :
+			magazin -= 1
+			shots += 1
+			gun_anime.play("shoot")
+			shot_.pitch_scale = randf_range(8, 1.2)
+			shot_.play()
+			instance = buleet.instantiate()
+			instance.position = bareel.global_position
+			instance.transform.basis = bareel.global_transform.basis
+			parent.get_parent().add_child(instance)
+		if !gun_anime2.is_playing() :
+			magazin -= 1
+			shots +=1
+			gun_anime2.play("shoot")
+			instance2 = buleet.instantiate()
+			instance2.position = bareel2.global_position
+			instance2.transform.basis = bareel2.global_transform.basis
+			parent.get_parent().add_child(instance2)
 
 
-func revolver_shot(delta) :
+
+func revolver_shot() :
 	if revolver_magazine >0 :
 		if !revolver_anime.is_playing() :
 			revolver_magazine -= 1
+			revolver_shots += 1
 			revolver_anime.play("shoot")
 			shot_revolver.pitch_scale = randf_range(0.8, 1.2)
 			shot_revolver.play()
@@ -135,15 +156,18 @@ func revolver_shot(delta) :
 			instance.transform.basis = revolver_bareel.global_transform.basis
 			instance.scale = Vector3(3,3,3)
 			parent.get_parent().add_child(instance)
-			camera.rotation.x = lerp(camera.rotation.x , camera.rotation.x + randf_range(0.25,0.5) , delta  )
+
+
 func revolver_reload() :
 	is_reloading = true
 	revolver_anime.play("reload")
 	reload_revolver.pitch_scale = randf_range(0.8, 1.2)
 	reload_revolver.play()
 	await get_tree().create_timer(1.5).timeout
-	revolver_magazine = revolver_magzin_number
+	revolver_magazine += revolver_shots
 	is_reloading = false
+	total_revolver_magazin -= revolver_shots
+	revolver_shots = 0
 
 func melee_hit() :
 	mele_hit.emit()
